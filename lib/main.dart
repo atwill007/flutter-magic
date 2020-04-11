99ca1f80ea8747530942264246385a11fa79b0ba
Jason <atwill007@163.com>
2020-04-11 21:22:47 +0800

    feat: theme change

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Welcome to Flutter',
+      // 主题控制您应用程序的外观和风格, 可以通过配置ThemeData类轻松更改应用程序的主题
+      theme: new ThemeData(
+        primaryColor: Colors.amber, // 更改应用程序的主题颜色
+        // accentColor: Colors.green,
+        // textTheme: TextTheme(body1: TextStyle(color: Colors.purple)),
+      ),
       home: new RandomWords(),
     );
   }
