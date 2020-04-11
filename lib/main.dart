101971c7ff050409a99832e010e177b4d04b50ab
Jason <atwill007@163.com>
2020-04-11 18:10:15 +0800

    feat: 使用 english_words 包做文字展示

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    debugPrint("wordPair: " + wordPair.toString());
+
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -12,7 +16,8 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          // child: new Text('Hello World'),
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
